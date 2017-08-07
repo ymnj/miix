45e9f774d99fa6254ed0467a11beda0e562cf7ca
Tom Hung <tom@tomhung.ca>
2017-08-06 17:59:29 -0700

    Add banner-sm assets. Complete banner section.

--- sections/section-front-banners.php
+++ sections/section-front-banners.php
@@ -6,9 +6,42 @@
       <h4>Interior Design + Decor</h4>
     </div>
   </div>
-  <div class="banner-info">
+  <div class="banner-info space">
     <p>At Miix Interiors, we specialize in professional, efficient and effective home staging and interior design service. Whether you’re a realtor or a homeowner, with our expertise and suite of interior designers, we quickly transform any space into a warm and inviting property for buyers.</p>
 
     <p>Since 2010, Miix Interiors has staged and furnished thousands of homes, condos, townhomes in the Greater Vancouver area. With our years of international industry experience and product knowledge, our designers draw on global design influences to restyle your home or property, polishing it to its true potential, to immediately engage buyers. Imagination is one thing, but in terms of property value, seeing really is believing! With our services, you will be able to maximize the value of your property and quickly turn a listing to a sale.  </p>
+  </div>
+
+  <div class="banner-image time">
+    <img src="<?php echo get_template_directory_uri() . '/assets/images/banner2-sm.jpg' ?>" alt="">
+    <div class="image-overlay text-center">
+      <h3>Time</h3>
+      <i class="icon-clock"></i>
+    </div>
+  </div>
+  <div class="banner-info">
+    <p>Experienced design team and efficient staging process allow Miix to complete the project within 24 hours.</p>
+  </div>
+
+  <div class="banner-image quality">
+    <img src="<?php echo get_template_directory_uri() . '/assets/images/banner3-sm.jpg' ?>" alt="">
+    <div class="image-overlay text-center">
+      <h3>Quality</h3>
+      <i class="icon-ok-circled2"></i>
+    </div>
+  </div>
+  <div class="banner-info">
+    <p>Variety and abundance of furniture and accessories collection to accommodate any house size and style.</p>
+  </div>
+
+  <div class="banner-image cost">
+    <img src="<?php echo get_template_directory_uri() . '/assets/images/banner4-sm.jpg' ?>" alt="">
+    <div class="image-overlay text-center">
+      <h3>Cost</h3>
+      <i class="icon-money"></i>
+    </div>
+  </div>
+  <div class="banner-info">
+    <p>Multiple service packages to fit your way of selling the property.</p>
   </div>
 </div>
